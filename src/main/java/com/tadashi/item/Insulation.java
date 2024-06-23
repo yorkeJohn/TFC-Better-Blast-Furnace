bce4dc4ab3fd5dd1469f45733c1233613843432b
John Yorke <yorkejohn02@gmail.com>
2024-06-23 10:59:03 -0300

    Cleanup

--- src/main/java/com/tadashi/item/Insulation.java
+++ src/main/java/com/tadashi/item/Insulation.java
@@ -4,10 +4,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
 import static com.tadashi.Registry.INSULATED_FIRE_BRICKS;
@@ -26,23 +24,21 @@ public class Insulation extends Item {
 
     @Override
     public @NotNull InteractionResult useOn(final UseOnContext ctx) {
-        final Level level = ctx.getLevel();
-        final BlockPos pos = ctx.getClickedPos();
-        final BlockState block = level.getBlockState(pos);
         final Player player = ctx.getPlayer();
-
         if (player == null) {
             return PASS;
         }
 
-        final ItemStack stack = player.getItemInHand(ctx.getHand());
-        if (!isBlock(block, FIRE_BRICKS.get())) {
+        final Level level = ctx.getLevel();
+        final BlockPos pos = ctx.getClickedPos();
+        if (!isBlock(level.getBlockState(pos), FIRE_BRICKS.get())) {
             return PASS;
         }
 
         level.setBlockAndUpdate(pos, INSULATED_FIRE_BRICKS.get().defaultBlockState());
-        stack.shrink(1);
+        player.getItemInHand(ctx.getHand()).shrink(1);
         playSound(level, pos, METAL_PLACE);
+
         return sidedSuccess(level.isClientSide);
     }
 }
