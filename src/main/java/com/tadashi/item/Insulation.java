61f4cfb54a2b704730db2146e8442aaaa5c15f78
John Yorke <yorkejohn02@gmail.com>
2024-06-22 23:21:49 -0300

    Initial commit

---
+++ src/main/java/com/tadashi/item/Insulation.java
@@ -0,0 +1,48 @@
+package com.tadashi.item;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+
+import static com.tadashi.Registry.INSULATED_FIRE_BRICKS;
+import static net.dries007.tfc.common.blocks.TFCBlocks.FIRE_BRICKS;
+import static net.dries007.tfc.util.Helpers.isBlock;
+import static net.dries007.tfc.util.Helpers.playSound;
+import static net.minecraft.sounds.SoundEvents.METAL_PLACE;
+import static net.minecraft.world.InteractionResult.PASS;
+import static net.minecraft.world.InteractionResult.sidedSuccess;
+
+public class Insulation extends Item {
+
+    public Insulation() {
+        super(new Item.Properties());
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(final UseOnContext ctx) {
+        final Level level = ctx.getLevel();
+        final BlockPos pos = ctx.getClickedPos();
+        final BlockState block = level.getBlockState(pos);
+        final Player player = ctx.getPlayer();
+
+        if (player == null) {
+            return PASS;
+        }
+
+        final ItemStack stack = player.getItemInHand(ctx.getHand());
+        if (isBlock(block, FIRE_BRICKS.get())) {
+            level.setBlockAndUpdate(pos, INSULATED_FIRE_BRICKS.get().defaultBlockState());
+            stack.shrink(1);
+            playSound(level, pos, METAL_PLACE);
+            return sidedSuccess(level.isClientSide);
+        }
+
+        return PASS;
+    }
+}
---
+++ src/main/java/com/tadashi/mixin/BlastFurnaceMixin.java
@@ -0,0 +1,56 @@
+package com.tadashi.mixin;
+
+import net.dries007.tfc.common.blocks.devices.BlastFurnaceBlock;
+import net.dries007.tfc.config.TFCConfig;
+import net.dries007.tfc.util.MultiBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Predicate;
+
+import static com.tadashi.Registry.INSULATED_FIRE_BRICKS;
+import static net.dries007.tfc.common.blocks.TFCBlocks.MOLTEN;
+import static net.dries007.tfc.util.Helpers.isBlock;
+
+@Mixin(BlastFurnaceBlock.class)
+public abstract class BlastFurnaceMixin {
+
+    @Accessor("BLAST_FURNACE_CHIMNEY")
+    private static MultiBlock getBlastFurnaceChimney() {
+        throw new AssertionError();
+    }
+
+    private static final MultiBlock BLAST_FURNACE_CHIMNEY = getBlastFurnaceChimney();
+
+    private static final MultiBlock INSULATED_CHIMNEY;
+
+    private static final Predicate<BlockState> IS_INSULATED = b -> isBlock(b, INSULATED_FIRE_BRICKS.get());
+
+    static {
+        INSULATED_CHIMNEY = new MultiBlock()
+                .match(new BlockPos(0, 0, 0), state -> state.isAir() || isBlock(state, MOLTEN.get()))
+                .match(new BlockPos(0, 0, 1), IS_INSULATED)
+                .match(new BlockPos(0, 0, -1), IS_INSULATED)
+                .match(new BlockPos(1, 0, 0), IS_INSULATED)
+                .match(new BlockPos(-1, 0, 0), IS_INSULATED);
+    }
+
+    @Inject(method = "getChimneyLevels", at = @At("HEAD"), cancellable = true)
+    private static void inject_getChimneyLevels(final Level level, final BlockPos pos, final CallbackInfoReturnable<Integer> cir) {
+        final int maxHeight = TFCConfig.SERVER.blastFurnaceMaxChimneyHeight.get();
+        for (int i = 0; i < maxHeight; i++) {
+            final BlockPos center = pos.above(i + 1);
+            if (!BLAST_FURNACE_CHIMNEY.test(level, center) && !INSULATED_CHIMNEY.test(level, center)) {
+                cir.setReturnValue(i);
+                return;
+            }
+        }
+        cir.setReturnValue(maxHeight);
+    }
+}
